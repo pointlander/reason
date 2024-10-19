e51d8970ef7fe408a27dca2a9592e9d6785746c3
Andrew Snodgrass <pointlander@gmail.com>
2024-10-19 14:27:37 -0500

    Learn iris data set

--- main.go
+++ main.go
@@ -10,7 +10,34 @@ import (
 	"embed"
 	"encoding/csv"
 	"io"
+	"math"
+	"math/rand"
 	"strconv"
+	"strings"
+
+	"github.com/pointlander/gradient/tf64"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+)
+
+const (
+	// B1 exponential decay of the rate for the first moment estimates
+	B1 = 0.8
+	// B2 exponential decay rate for the second-moment estimates
+	B2 = 0.89
+	// Eta is the learning rate
+	Eta = .01
+)
+
+const (
+	// StateM is the state for the mean
+	StateM = iota
+	// StateV is the state for the variance
+	StateV
+	// StateTotal is the total number of states
+	StateTotal
 )
 
 //go:embed iris.zip
@@ -81,6 +108,135 @@ func Load() []Fisher {
 }
 
 func main() {
+	rng := rand.New(rand.NewSource(1))
 	iris := Load()
-	_ = iris
+
+	set := tf64.NewSet()
+	set.Add("w1", 4, 4)
+	set.Add("b1", 4)
+	set.Add("w2", 4, 3)
+	set.Add("b2", 3)
+
+	for i := range set.Weights {
+		w := set.Weights[i]
+		if strings.HasPrefix(w.N, "b") {
+			w.X = w.X[:cap(w.X)]
+			w.States = make([][]float64, StateTotal)
+			for i := range w.States {
+				w.States[i] = make([]float64, len(w.X))
+			}
+			continue
+		}
+		factor := math.Sqrt(float64(w.S[0]))
+		for i := 0; i < cap(w.X); i++ {
+			w.X = append(w.X, rng.NormFloat64()*factor)
+		}
+		w.States = make([][]float64, StateTotal)
+		for i := range w.States {
+			w.States[i] = make([]float64, len(w.X))
+		}
+	}
+
+	others := tf64.NewSet()
+	others.Add("input", 4)
+	others.Add("output", 3)
+
+	for i := range others.Weights {
+		w := others.Weights[i]
+		w.X = w.X[:cap(w.X)]
+	}
+
+	l1 := tf64.Sigmoid(tf64.Add(tf64.Mul(set.Get("w1"), others.Get("input")), set.Get("b1")))
+	l2 := tf64.Sigmoid(tf64.Add(tf64.Mul(set.Get("w2"), l1), set.Get("b2")))
+	loss := tf64.Quadratic(l2, others.Get("output"))
+
+	points := make(plotter.XYs, 0, 8)
+	for i := 0; i < 33*len(iris); i++ {
+		pow := func(x float64) float64 {
+			y := math.Pow(x, float64(i+1))
+			if math.IsNaN(y) || math.IsInf(y, 0) {
+				return 0
+			}
+			return y
+		}
+
+		others.Zero()
+		index := rng.Intn(len(iris))
+		input := others.ByName["input"].X
+		for j := range input {
+			input[j] = iris[index].Measures[j]
+		}
+		output := others.ByName["output"].X
+		for j := range output {
+			output[j] = 0
+		}
+		output[Labels[iris[index].Label]] = 1
+
+		set.Zero()
+		cost := tf64.Gradient(loss).X[0]
+
+		norm := 0.0
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				norm += d * d
+			}
+		}
+		norm = math.Sqrt(norm)
+		b1, b2 := pow(B1), pow(B2)
+		if norm > 1 {
+			scaling := 1 / norm
+			for _, w := range set.Weights {
+				for l, d := range w.D {
+					g := d * scaling
+					m := B1*w.States[StateM][l] + (1-B1)*g
+					v := B2*w.States[StateV][l] + (1-B2)*g*g
+					w.States[StateM][l] = m
+					w.States[StateV][l] = v
+					mhat := m / (1 - b1)
+					vhat := v / (1 - b2)
+					if vhat < 0 {
+						vhat = 0
+					}
+					w.X[l] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+				}
+			}
+		} else {
+			for _, w := range set.Weights {
+				for l, d := range w.D {
+					g := d
+					m := B1*w.States[StateM][l] + (1-B1)*g
+					v := B2*w.States[StateV][l] + (1-B2)*g*g
+					w.States[StateM][l] = m
+					w.States[StateV][l] = v
+					mhat := m / (1 - b1)
+					vhat := v / (1 - b2)
+					if vhat < 0 {
+						vhat = 0
+					}
+					w.X[l] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+				}
+			}
+		}
+
+		points = append(points, plotter.XY{X: float64(i), Y: float64(cost)})
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "epochs.png")
+	if err != nil {
+		panic(err)
+	}
 }
